39ab97665bf13d136047f37a27b51aaf65ac9ae1
Artur Kukhotskovolets <114314218+Archichil@users.noreply.github.com>
2025-02-12 18:07:47 +0300

    Initial Commit

---
+++ avitotech-assignment/avitotech_assignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  avitotech_assignmentApp.swift
+//  avitotech-assignment
+//
+//  Created by Archichil on 12.02.25.
+//
+
+import SwiftUI
+
+@main
+struct avitotech_assignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
